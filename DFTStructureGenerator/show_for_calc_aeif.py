a94d84c534e6012ea65ed53faaf5bb37e9b5d1d6
Jackie <jackie_lijiaqi@outlook.com>
2025-11-11 19:41:00 +0800

    modified:   DFTStructureGenerator/show_for_calc_aeif.py

--- DFTStructureGenerator/show_for_calc_aeif.py
+++ DFTStructureGenerator/show_for_calc_aeif.py
@@ -10,14 +10,12 @@ from DFTStructureGenerator import DFThandle, xtb_process, mol_manipulation, gend
 def Calc_AEIF_(mol, charges, excluded_atoms_ids=[], num_per_axis=20, radius=8, count_per_axis = [2,2,2]):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    # 非均匀格点积分
     table = Chem.rdchem.GetPeriodicTable()
     mol_conformers = mol.GetConformers()
     assert len(mol_conformers) == 1
     atomicnum_list = [atom.GetAtomicNum() for atom in mol.GetAtoms()]
     geom = mol_conformers[0].GetPositions()
 
-    # 正方体边长和总点数
     num = num_per_axis
     radius /= 2
     cube_length = 2 * radius
@@ -25,33 +23,25 @@ def Calc_AEIF_(mol, charges, excluded_atoms_ids=[], num_per_axis=20, radius=8, c
     count_num = count_per_axis[0] * count_per_axis[1] * count_per_axis[2]
     counts = np.zeros(count_num, dtype=np.int32)
     counts_num_each = np.zeros(count_num, dtype=np.int32)
-
-    # 生成均匀的网格点
     x = np.linspace(0.1 -radius, radius - 0.1, num)
     y = x; z = x
-    # 生成点
     points = np.array(np.meshgrid(x, y, z)).T.reshape(-1, 3)
     charge_energy = np.zeros(total_points, dtype=bool)
-    # 计算每个点到每个原子中心的距离
     for atom_id, (sphere_center, atom_charge) in enumerate(zip(geom, charges)):
         if atom_id in excluded_atoms_ids:
             continue
         translated_points = (points - sphere_center)
         distances = np.linalg.norm(translated_points, axis=1)
         charge_energy = charge_energy + atom_charge / distances
-    # 归一化电荷能
     normal_charge_energy = (charge_energy - np.min(charge_energy)) / (np.max(charge_energy) - np.min(charge_energy))
-    # 绘制格点
     target_idx = [idx for idx, charge_energy_ in enumerate(charge_energy) if charge_energy_ > 0.1]
     xs, ys, zs = zip(*points[target_idx])
     # ax.scatter(xs, ys, zs, c=[(each, 1 - each, 0) for each in normal_charge_energy[target_idx]], s=10)
 
-    # 设置坐标轴范围
     ax.set_xlim([-radius, radius])
     ax.set_ylim([-radius, radius])
     ax.set_zlim([-radius, radius])
 
-    # 设置坐标轴标签
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
@@ -73,8 +63,6 @@ def Calc_AEIF_(mol, charges, excluded_atoms_ids=[], num_per_axis=20, radius=8, c
     for each_a in a:
         for each_b in b:
             ax.plot(each_a, each_b, line_a, color='gray', linestyle='--')
-    # 绘制坐标轴平面
-    # 显示图形
     ax.axis("off")
     # plt.savefig("test.svg", useSVG=True)
     plt.show()
